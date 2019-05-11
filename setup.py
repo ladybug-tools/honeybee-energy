5cc11ee9a435c6fd7179077df38ece3017b6fac5
Mostapha <sadeghipour@gmail.com>
2019-05-12 02:11:59 +0430

    feat(*): init repository
    added initial files to start the repository.

---
+++ setup.py
@@ -0,0 +1,27 @@
+import re
+import setuptools
+import sys
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="honeybee-energy",
+    use_scm_version = True,
+    setup_requires=['setuptools_scm'],
+    author="Ladybug Tools",
+    author_email="info@ladybug.tools",
+    description="Energy simulation library for honeybee.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ladybug-tools/honeybee-energy",
+    packages=setuptools.find_packages(exclude=["tests"]),
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent"
+    ],
+)
