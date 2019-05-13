83ff993ce0f72b77449af31e198f28f9cceb1add
Mostapha <sadeghipour@gmail.com>
2019-05-14 01:14:37 +0430

    feat(boundary condition): add boundary condition and finish to_idf
    added boundarycondition module in order to finish face to idf property for honeybee Face.

--- honeybee_energy/faceutil.py
+++ honeybee_energy/faceutil.py
@@ -1,24 +1,32 @@
 @property
 def face_to_idf(self):
     """generate face idf representation."""
-    # BuildingSurface:Detailed,
-    #     Zn001:Wall001,           !- Name
-    #     Wall,                    !- Surface Type
-    #     R13WALL,                 !- Construction Name
-    #     Main Zone,               !- Zone Name
-    #     Outdoors,                !- Outside Boundary Condition
-    #     ,                        !- Outside Boundary Condition Object
-    #     SunExposed,              !- Sun Exposure
-    #     WindExposed,             !- Wind Exposure
-    #     0.5000000,               !- View Factor to Ground
-    #     4,                       !- Number of Vertices
-    #     0,0,4.572000,  !- X,Y,Z ==> Vertex 1 {m}
-    #     0,0,0,  !- X,Y,Z ==> Vertex 2 {m}
-    #     15.24000,0,0,  !- X,Y,Z ==> Vertex 3 {m}
-    #     15.24000,0,4.572000;  !- X,Y,Z ==> Vertex 4 {m}
-    return 'BuildingSurface:Detailed,\n\t%s,\n\t%s,\n\t%s;' % (
-        self.name,
-        self.properties.face_type,
-        self.properties.energy.construction.name,
-        # self.parent.name
+    #     TODO(): Check self.geo_type first. This only works for a Face. Fails for
+    #     PolyFace, etc
+    ep = self.properties.energy
+
+    idf_string = 'BuildingSurface:Detailed,' \
+        '\n\t%s,\t!- Name' \
+        '\n\t%s,\t!- Surface Type' \
+        '\n\t%s,\t!- Construction Name' \
+        '\n\t%s,\t!- Zone Name' \
+        '\n\t%s,\t!- Outside Boundary Condition' \
+        '\n\t%s,\t!- Outside Boundary Condition Object' \
+        '\n\t%s,\t!- Sun Exposure' \
+        '\n\t%s,\t!- Wind Exposure' \
+        '\n\t%s,\t!- View Factor to Ground' \
+        '\n\t%d,\t!- Number of Vertices' \
+        '\n\t%s;' % (
+            self.name,
+            self.properties.face_type.name,
+            ep.construction.name,
+            self.parent.name if self.parent else 'unknown',
+            ep.boundary_condition.name,
+            ep.boundary_condition.boundary_condition_object_idf,
+            ep.boundary_condition.sun_exposure_idf,
+            ep.boundary_condition.wind_exposure_idf,
+            ep.boundary_condition.view_factor,
+            len(self.vertices),
+            ',\n\t'.join('%f, %f, %f' % (v[0], v[1], v[2]) for v in self.vertices)
         )
+    return idf_string
--- honeybee_energy/properties.py
+++ honeybee_energy/properties.py
@@ -1,12 +1,13 @@
 """Energy Properties."""
+from .boundarycondition import BoundaryConditions
 
 
 class EnergyProperties(object):
     """Energy Properties for Honeybee geometry."""
 
     def __init__(self, srf_type):
-        # set default construction based on srf_type
         self.construction = srf_type.energy_construction
+        self.boundary_condition = BoundaryConditions().outdoors
 
     @property
     def construction(self):
@@ -25,15 +26,21 @@ class EnergyProperties(object):
         return self._construction.materials
 
     @property
+    def boundary_condition(self):
+        """Get and set boundary condition."""
+        return self._boundary_condition
+
+    @boundary_condition.setter
+    def boundary_condition(self, bc):
+        self._boundary_condition = bc
+
+    @property
     def to_dict(self):
         """Return energy properties as a dictionary."""
-        return {
-            'properties': {
-                'energy': {
-                    'construction': self.construction.to_dict
-                }
-            }
-        }
+        base = {'energy': {}}
+        base['energy'].update(self.boundary_condition.to_dict)
+        base['energy']['construction'] = self.construction.to_dict
+        return base
 
     def __repr__(self):
         return 'EnergyProperties:%s' % self.construction.name
