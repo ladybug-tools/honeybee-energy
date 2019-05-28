2ef0ccbd53b41849137e11757e2e61e989279c62
Mostapha <sadeghipour@gmail.com>
2019-05-28 15:34:43 -0400

    refactor(writer): rename faceutil to writer
    fixes #2

--- honeybee_energy/_extend_honeybee.py
+++ honeybee_energy/_extend_honeybee.py
@@ -1,7 +1,7 @@
 from honeybee.properties import Properties
 import honeybee.writer as writer
 from .properties import EnergyProperties
-from .faceutil import face_to_idf
+from .writer import face_to_idf
 
 # add energy properties to Properties class
 Properties.energy =  property(lambda self: EnergyProperties(self.face_type))
