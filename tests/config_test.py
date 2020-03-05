5774a257eb4e83dd6dc2de2ba21f2d5eb06a7968
Chris <chris@ladybug.tools>
2020-03-05 10:52:50 -0500

    feat(config): Add the ability to sense version of EnergyPlus/OpenStudio

--- tests/config_test.py
+++ tests/config_test.py
@@ -1,5 +1,6 @@
 # coding=utf-8
 from honeybee_energy.config import folders
+from honeybee_energy.writer import energyplus_idf_version
 
 import pytest
 
@@ -8,9 +9,17 @@ def test_config_init():
     """Test the initialization of the config module and basic properties."""
     assert hasattr(folders, 'energyplus_path')
     assert folders.energyplus_path is None or isinstance(folders.energyplus_path, str)
+    assert hasattr(folders, 'energyplus_exe')
+    assert folders.energyplus_exe is None or isinstance(folders.energyplus_exe, str)
+    assert hasattr(folders, 'energyplus_version')
+    assert folders.energyplus_version is None or isinstance(folders.energyplus_version, tuple)
 
     assert hasattr(folders, 'openstudio_path')
     assert folders.openstudio_path is None or isinstance(folders.openstudio_path, str)
+    assert hasattr(folders, 'openstudio_exe')
+    assert folders.openstudio_exe is None or isinstance(folders.openstudio_exe, str)
+    assert hasattr(folders, 'openstudio_version')
+    assert folders.openstudio_version is None or isinstance(folders.openstudio_version, tuple)
 
     assert hasattr(folders, 'energy_model_measure_path')
     assert folders.energy_model_measure_path is None or \
@@ -24,3 +33,10 @@ def test_config_init():
     assert isinstance(folders.programtype_lib, str)
 
     assert isinstance(folders.config_file, str)
+
+
+def test_writer_version_idf():
+    """Test the energyplus_idf_version method."""
+    assert energyplus_idf_version() is None or \
+        isinstance(energyplus_idf_version(), str)
+    assert isinstance(energyplus_idf_version((9, 2, 0)), str)
