f5e632cc339a77dce7555dbc3fd2e75358a6c2c1
trevor <63176386+TfedUD@users.noreply.github.com>
2021-09-17 15:17:08 -0400

    added docstring to userdatadict fixture

--- tests/fixtures/userdata_fixtures.py
+++ tests/fixtures/userdata_fixtures.py
@@ -2,6 +2,9 @@ import pytest
 
 @pytest.fixture
 def userdatadict():
+    """ A fixture to keep from cluttering tests.py with This 
+    'all(almost all) the types dict', and also to type less """
+       
     udd = {"str_key":"str_val",
                     "float_key": 0.01,
                     "t_key":True,
