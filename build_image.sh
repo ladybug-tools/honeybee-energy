563aeb853c594532fd1a5e9cff8e34dfeb03cb56
Chris Mackey <chris@ladybug.tools>
2021-09-21 15:25:21 -0400

    fix(docker): Include lbt-measures in the docker image

--- build_image.sh
+++ build_image.sh
@@ -17,6 +17,17 @@ curl -SL -o ${OPENSTUDIO_TAR_FILENAME} ${OPENSTUDIO_DOWNLOAD_URL}
 tar zxvf ${OPENSTUDIO_TAR_FILENAME}
 mv OpenStudio-*-Ubuntu-*/ ${OPENSTUDIO_FILENAME}
 
+# Get lbt-measures
+
+export LBT_MEASURES_VERSION="0.2.0"
+export LBT_MEASURES_URL="https://github.com/ladybug-tools/lbt-measures/archive/v${LBT_MEASURES_VERSION}.tar.gz"
+export LBT_MEASURES_TAR='lbt-measures.tar.gz'
+export LBT_MEASURES_FILENAME='measures-gem'
+
+curl -SL -o ${LBT_MEASURES_TAR} ${LBT_MEASURES_URL}
+tar zxvf ${LBT_MEASURES_TAR}
+mv lbt-measures/lib-*/ ${LBT_MEASURES_FILENAME}
+
 # Get the gem
 
 export HONEYBEE_OPENSTUDIO_GEM_VERSION="2.23.6"
@@ -26,12 +37,13 @@ export HONEYBEE_GEM_FILENAME='honeybee-gem'
 
 curl -SL -o ${HONEYBEE_OPENSTUDIO_GEM_TAR} ${HONEYBEE_OPENSTUDIO_GEM_URL}
 tar zxvf ${HONEYBEE_OPENSTUDIO_GEM_TAR}
-mv honeybee-openstudio-gem-*/ ${HONEYBEE_GEM_FILENAME}
+mv honeybee-openstudio-gem/lib-*/ ${HONEYBEE_GEM_FILENAME}
 
 docker build . \
   -t $CONTAINER_NAME:$TAG \
   --build-arg OPENSTUDIO_VERSION=${OPENSTUDIO_VERSION} \
   --build-arg OPENSTUDIO_FILENAME=${OPENSTUDIO_FILENAME} \
+  --build-arg LBT_MEASURES_FILENAME=${LBT_MEASURES_FILENAME} \
   --build-arg HONEYBEE_GEM_FILENAME=${HONEYBEE_GEM_FILENAME}
 
 if [[ "${3}" == 'true' ]]; then
