883fc6617cb240875be6abce47cf937e801e1996
Chris Mackey <chris@ladybug.tools>
2022-05-18 15:58:27 -0400

    fix(openstudio): Update OpenStudio version used in docker and test

--- build_image.sh
+++ build_image.sh
@@ -8,8 +8,8 @@ export TAG="${2:?$error_help}"
 
 # Get OpenStudio
 
-export OPENSTUDIO_VERSION='3.3.0'
-export OPENSTUDIO_DOWNLOAD_URL='https://github.com/NREL/OpenStudio/releases/download/v3.3.0/OpenStudio-3.3.0+ad235ff36e-Ubuntu-18.04.tar.gz'
+export OPENSTUDIO_VERSION='3.4.0'
+export OPENSTUDIO_DOWNLOAD_URL='https://github.com/NREL/OpenStudio/releases/download/v3.4.0/OpenStudio-3.4.0+4bd816f785-Windows.tar.gz'
 export OPENSTUDIO_TAR_FILENAME='openstudio.tar.gz'
 export OPENSTUDIO_FILENAME='openstudio'
 
