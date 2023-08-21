a9958703e533666bf983d35038cbdefc7b73441e
Chris Mackey <chris@ladybug.tools>
2023-08-21 13:03:55 -0700

    fix(edit): Catch the case of auto-generated schedules in reset IDs

--- tests/cli_edit_test.py
+++ tests/cli_edit_test.py
@@ -19,3 +19,19 @@ def test_reset_resource_ids():
     old_id = '2013::ClimateZone5::SteelFramed'
     assert new_con_set.identifier.startswith(old_id)
     assert new_con_set.identifier != old_id
+
+
+def test_reset_resource_ids_humidification():
+    runner = CliRunner()
+    input_hb_model = './tests/json/custom_resources.hbjson'
+
+    c_args = [input_hb_model, '-uuid']
+    result = runner.invoke(reset_resource_ids, c_args)
+    assert result.exit_code == 0
+    model_dict = json.loads(result.output)
+    new_model = Model.from_dict(model_dict)
+    
+    assert len(new_model.properties.energy.program_types) == 9
+    assert len(new_model.properties.energy.schedules) == 51
+    assert len(new_model.properties.energy.construction_sets) == 1
+    assert len(new_model.properties.energy.constructions) == 7
