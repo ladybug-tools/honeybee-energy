b7b8cad967f5ba8bf0562cc1538a523e56c3f187
Chris Mackey <chris@ladybug.tools>
2023-10-29 12:00:18 -0700

    fix(lib): Ensure that unassigned schedules do not cause issues

--- honeybee_energy/lib/_loadconstructions.py
+++ honeybee_energy/lib/_loadconstructions.py
@@ -101,9 +101,11 @@ def load_construction_object(
             except AttributeError:  # construction without materials
                 pass
             if isinstance(constr, (WindowConstructionShade, WindowConstructionDynamic)):
-                misc_sch.append(constr.schedule)
+                if constr.schedule is not None:
+                    misc_sch.append(constr.schedule)
             elif isinstance(constr, AirBoundaryConstruction):
-                misc_sch.append(constr.air_mixing_schedule)
+                if constr.air_mixing_schedule is not None:
+                    misc_sch.append(constr.air_mixing_schedule)
         if constr is not None:
             lock_and_check_construction(constr)
             if isinstance(constr, _opa_types):
