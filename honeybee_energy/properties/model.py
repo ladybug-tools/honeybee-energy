44a5d4c3e8d101e15e10cce0647548fffdfaa0ba
Chris Mackey <chris@ladybug.tools>
2026-03-16 10:47:20 -0700

    fix(model): Fix bug in the check for multiple HVAC rooms

--- honeybee_energy/properties/model.py
+++ honeybee_energy/properties/model.py
@@ -1091,7 +1091,7 @@ class ModelEnergyProperties(object):
                 continue
             hvacs = [r.properties.energy.hvac for r in rooms
                      if r.properties.energy.hvac is not None]
-            if len(hvacs) > 1:
+            if len(set(hvacs)) > 1:
                 invalid_zones[zone] = [rooms, hvacs]
 
         # if multiple HVACs in the same zone were found, then report the issue
